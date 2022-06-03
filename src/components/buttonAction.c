6fa9ddc0365fd30e8a9464ef6365eda8507a19a2
Jonathandharma <99466172+Jonathandharma@users.noreply.github.com>
2022-06-03 22:20:25 +0700

    buttonAction

---
+++ src/components/buttonAction.c
@@ -0,0 +1,17 @@
+<div class="aktivitas container-fluid mb-3 mt-3 text-center">
+            <div class="row">
+                <div class="groupBtn col-md-12 mb-2">
+                    <button id="btnMakan" class="btn btn-primary" onclick="makanGambar(); setTimeout(Makan, 2000); disable(); ">Makan & Minum</button>
+                </div>
+                <div class="groupBtn col-md-12 mb-2">
+                    <button id="btnTidur" class="btn btn-primary" onclick="tidurGambar(); setTimeout(Tidur, 2000); disable();">Tidur</button>
+                </div>
+                <div class="groupBtn col-md-12 mb-2">
+                    <button id="btnGame" class="btn btn-primary" onclick="gameGambar(); setTimeout(Main, 2000); disable();">Main Game</button>
+                </div>
+                <div class="groupBtn col-md-12 mb-2">
+                    <button id="btnBljr" class="btn btn-primary" onclick="belajarGambar(); setTimeout(Belajar, 2000); disable();">Belajar</button>
+                </div>
+            </div>
+        </div>
+    </div>
